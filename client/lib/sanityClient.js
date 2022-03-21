a5eb66338accefbdf379ed7934211169ae60e525
michael matos <mbaezmichael@gmail.com>
2022-03-20 22:19:14 -0400

    Update sanityClient.js

--- client/lib/sanityClient.js
+++ client/lib/sanityClient.js
@@ -1,10 +1,10 @@
 import sanityClient from '@sanity/client'
 
 export const client = sanityClient({
-    projectId: 'suioxxl7',
+    projectId: '',
     dataset: 'production',
     apiVersion: 'v1',
     token:
-      'skW2DGNWg2dkxxfFn7K7wcIuQ3VH190JMUxNKXCgziX0QqfJ2QVYuwhNRcYmrOdc5gv6F2ohsUeqddhbXnk7Ucf6hlmtKyWDngEvzFhzG5kcPMkcUvReKne7HeIYB7OGQpmUBuy5eddSxqFDSf70yPPNNg3yFIWtBYQ2Bde3MPfnPCuXYCeV',
+      '',
     useCdn: false,
   })
